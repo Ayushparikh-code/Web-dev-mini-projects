461d14c9507fc85e571ff51cf01380fac72f3762
Khushi <khushipurwar4@gmail.com>
2021-07-06 13:18:16 +0530

    Changes done accordingly

--- Text Analyzer/script.js
+++ Text Analyzer/script.js
@@ -8,44 +8,44 @@ const textArea = document.querySelector(".text-entry textarea");
 const btn = document.getElementById("process-btn");
 
 
-btn.addEventListener('click' , ()=>{
-    let text = textArea.value;
-    charCount.innerHTML = text.length;
-    wordCount.innerHTML = countWords(text);
-    sentences.innerHTML =countSentences(text);
-    punctuations.innerHTML = puncCount(text);
-    spaces.innerHTML = text.split(" ").length -1;
+btn.addEventListener('click', () => {
+	let text = textArea.value;
+	charCount.innerHTML = text.length;
+	wordCount.innerHTML = countWords(text);
+	sentences.innerHTML = countSentences(text);
+	punctuations.innerHTML = puncCount(text);
+	spaces.innerHTML = text.split(" ").length - 1;
 })
 
-function countWords(text){
+function countWords(text) {
 
-    let tempText = text.replace(/[.,!%&*;:'"-()]/g, "");
-    tempText= tempText.split(/\n/);
+	let tempText = text.replace(/[.,!%&*;:'"-()]/g, "");
+	tempText = tempText.split(/\n/);
 
-    let tempList = [];
-    tempText.forEach(word => tempList.push(word.split(" ")));
-    
-    function extract(arr){
-      return arr.reduce( (wordList, word) =>{
-        return wordList.concat(Array.isArray(word) ? extract(word) : word)
-      }, []);
-    }
+	let tempList = [];
+	tempText.forEach(word => tempList.push(word.split(" ")));
 
-    let wordList = extract(tempList);
-    return wordList.filter( char => char != '').length;
+	function extract(arr) {
+		return arr.reduce((wordList, word) => {
+			return wordList.concat(Array.isArray(word) ? extract(word) : word)
+		}, []);
+	}
+
+	let wordList = extract(tempList);
+	return wordList.filter(char => char != '').length;
 }
 
-function countSentences(text){
+function countSentences(text) {
 
-  const regex = /[\w|\)][.?!](\s|$)/g;
-  let senCount = text.match(regex);
-  return senCount ? senCount.length : 0;   
-   
+	const regex = /[\w|\)][.?!](\s|$)/g;
+	let senCount = text.match(regex);
+	return senCount ? senCount.length : 0;
+
 }
 
-function puncCount(text){
-    const regex= /[.,?;:'"!-(){}]/g;
-    let puntuationCount = text.match(regex);
-    return puntuationCount ? puntuationCount.length: 0;
+function puncCount(text) {
+	const regex = /[.,?;:'"!-(){}]/g;
+	let puntuationCount = text.match(regex);
+	return puntuationCount ? puntuationCount.length : 0;
 }
 
