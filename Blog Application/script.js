000a0a2cbe01cbab6015b64f5d8307440ef86ef4
Khushi Purwar <52875298+khushi-purwar@users.noreply.github.com>
2021-07-07 19:10:47 +0530

    Update script.js

--- Blog Application/script.js
+++ Blog Application/script.js
@@ -6,7 +6,13 @@ let globalStore = [];
 
 // -----------------------------------------------------
 // a function for creating a new card
-const newCard = ({ id, imageUrl, blogTitle, blogType, blogDescription }) => `<div class="col-lg-4 col-md-6" id=${id}>
+const newCard = ({
+	id,
+	imageUrl,
+	blogTitle,
+	blogType,
+	blogDescription
+}) => `<div class="col-lg-4 col-md-6" id=${id}>
 <div class="card m-2">
   <div class="card-header d-flex justify-content-end gap-2">
     <button type="button" class="btn btn-outline-success" id="${id}" onclick="editCard.apply(this, arguments)"><i class="fas fa-pencil-alt" id="${id}" onclick="editCard.apply(this, arguments)"></i></button>
@@ -30,185 +36,191 @@ const newCard = ({ id, imageUrl, blogTitle, blogType, blogDescription }) => `<di
 // --------------------------------------------------
 const loadData = () => {
 
-  // access localstorage
-  // localStorage.getItem("blog") ===  localStorage.blog
-  const getInitialData = localStorage.blog;  // if null, then
-  if (!getInitialData) return;
+	// access localstorage
+	// localStorage.getItem("blog") ===  localStorage.blog
+	const getInitialData = localStorage.blog; // if null, then
+	if (!getInitialData) return;
 
-  // convert stringified-object to object
-  const { cards } = JSON.parse(getInitialData);
+	// convert stringified-object to object
+	const {
+		cards
+	} = JSON.parse(getInitialData);
 
-  // map around the array to generate HTML card and inject it to DOM
-  cards.map((blogObject) => {
-    const createNewBlog = newCard(blogObject);
-    blogContainer.insertAdjacentHTML("beforeend", createNewBlog);
-    globalStore.push(blogObject);
-  });
+	// map around the array to generate HTML card and inject it to DOM
+	cards.map((blogObject) => {
+		const createNewBlog = newCard(blogObject);
+		blogContainer.insertAdjacentHTML("beforeend", createNewBlog);
+		globalStore.push(blogObject);
+	});
 };
 
 const updateLocalStorage = () => {
-  localStorage.setItem("blog", JSON.stringify({ cards: globalStore }))
+	localStorage.setItem("blog", JSON.stringify({
+		cards: globalStore
+	}))
 }
 
 //  function for save changes----------------------------------------
 
 // create a function which will trigerred on clicking on save changes in the modal
 const saveChanges = () => {
-  const blogData = {
-    id: `${Date.now()}`,   // generating a unique id for each card
-    imageUrl: document.getElementById('imageurl').value,
-    blogTitle: document.getElementById('title').value,
-    blogType: document.getElementById('type').value,
-    blogDescription: document.getElementById('description').value
-  };
+	const blogData = {
+		id: `${Date.now()}`, // generating a unique id for each card
+		imageUrl: document.getElementById('imageurl').value,
+		blogTitle: document.getElementById('title').value,
+		blogType: document.getElementById('type').value,
+		blogDescription: document.getElementById('description').value
+	};
 
-  const createNewBlog = newCard(blogData);
-  blogContainer.insertAdjacentHTML("beforeend", createNewBlog);
+	const createNewBlog = newCard(blogData);
+	blogContainer.insertAdjacentHTML("beforeend", createNewBlog);
 
-  globalStore.push(blogData);
+	globalStore.push(blogData);
 
-  //  API  -> add t localStorage
-  updateLocalStorage()
-  // provide some unique identification, i.e key, here key is "blog", 
+	//  API  -> add t localStorage
+	updateLocalStorage()
+	// provide some unique identification, i.e key, here key is "blog", 
 
 };
 
 // function for deleting a card -------------------
 
 const deleteCard = (event) => {
-  // id
-  event = window.event;
-  const targetID = event.target.id;
-  const tagname = event.target.tagName;       // BUTTON OR I
+	// id
+	event = window.event;
+	const targetID = event.target.id;
+	const tagname = event.target.tagName; // BUTTON OR I
 
-  // assign the same id of card to button also
+	// assign the same id of card to button also
 
-  // search the globalStore, remove the object which matches with the id
-  globalStore = globalStore.filter((blogObject) => blogObject.id !== targetID);
+	// search the globalStore, remove the object which matches with the id
+	globalStore = globalStore.filter((blogObject) => blogObject.id !== targetID);
 
-  updateLocalStorage();
+	updateLocalStorage();
 
-  // access DOM to remove them
+	// access DOM to remove them
 
-  if (tagname === "BUTTON") {
-    // task__container
-    return blogContainer.removeChild(
-      event.target.parentNode.parentNode.parentNode // col-lg-4
-    );
-  }
+	if (tagname === "BUTTON") {
+		// task__container
+		return blogContainer.removeChild(
+			event.target.parentNode.parentNode.parentNode // col-lg-4
+		);
+	}
 
-  // else
-  // blog__container
-  return blogContainer.removeChild(
-    event.target.parentNode.parentNode.parentNode.parentNode // col-lg-4
-  );
+	// else
+	// blog__container
+	return blogContainer.removeChild(
+		event.target.parentNode.parentNode.parentNode.parentNode // col-lg-4
+	);
 };
 
 // function for editing
 const editCard = (event) => {
 
-  event = window.event;
-  const targetID = event.target.id;
-  const tagname = event.target.tagName;
+	event = window.event;
+	const targetID = event.target.id;
+	const tagname = event.target.tagName;
 
-  let parentElement;
-  if (tagname === "BUTTON")
-  {
-    parentElement = event.target.parentNode.parentNode;
-    
-  }
-  else {
-    parentElement = event.target.parentNode.parentNode.parentNode;
-  }
+	let parentElement;
+	if (tagname === "BUTTON") {
+		parentElement = event.target.parentNode.parentNode;
 
-  let blogTitle = parentElement.childNodes[5].childNodes[1];
-  let blogDescription = parentElement.childNodes[5].childNodes[3];
-  let blogType = parentElement.childNodes[5].childNodes[5];
-  let submitBtn = parentElement.childNodes[7].childNodes[1];
-  // console.log(taskTitle, taskDescription, taskType);
+	} else {
+		parentElement = event.target.parentNode.parentNode.parentNode;
+	}
 
-  // setAttributes
-  blogTitle.setAttribute("contenteditable","true");
-  
-  blogDescription.setAttribute("contenteditable","true");
-  blogType.setAttribute("contenteditable","true");
-  submitBtn.setAttribute(
-    "onclick", 
-    "saveEditChanges.apply(this, arguments)"
-    );
-    submitBtn.innerHTML = "Save Changes";
+	let blogTitle = parentElement.childNodes[5].childNodes[1];
+	let blogDescription = parentElement.childNodes[5].childNodes[3];
+	let blogType = parentElement.childNodes[5].childNodes[5];
+	let submitBtn = parentElement.childNodes[7].childNodes[1];
+	// console.log(taskTitle, taskDescription, taskType);
 
-    //  modal removed
-  submitBtn.removeAttribute("data-bs-toggle");
-  submitBtn.removeAttribute("data-bs-target");
+	// setAttributes
+	blogTitle.setAttribute("contenteditable", "true");
 
-  
+	blogDescription.setAttribute("contenteditable", "true");
+	blogType.setAttribute("contenteditable", "true");
+	submitBtn.setAttribute(
+		"onclick",
+		"saveEditChanges.apply(this, arguments)"
+	);
+	submitBtn.innerHTML = "Save Changes";
+
+	//  modal removed
+	submitBtn.removeAttribute("data-bs-toggle");
+	submitBtn.removeAttribute("data-bs-target");
+
+
 
 }
 
-const saveEditChanges = (event)=>{
+const saveEditChanges = (event) => {
 
-  event = window.event;
-  const targetID = event.target.id;
-  const tagname = event.target.tagName;
+	event = window.event;
+	const targetID = event.target.id;
+	const tagname = event.target.tagName;
 
-  let parentElement;
-  if (tagname === "BUTTON")
-  {
-    parentElement = event.target.parentNode.parentNode;
-    
-  }
-  else {
-    parentElement = event.target.parentNode.parentNode.parentNode;
-  }
+	let parentElement;
+	if (tagname === "BUTTON") {
+		parentElement = event.target.parentNode.parentNode;
 
-  let blogTitle = parentElement.childNodes[5].childNodes[1];
-  let blogDescription = parentElement.childNodes[5].childNodes[3];
-  let blogType = parentElement.childNodes[5].childNodes[5];
-  let submitBtn = parentElement.childNodes[7].childNodes[1];
+	} else {
+		parentElement = event.target.parentNode.parentNode.parentNode;
+	}
 
-  const updatedData = {
-    
-   blogTitle : blogTitle.innerHTML,
-   blogDescription : blogDescription.innerHTML,
-   blogType : blogType.innerHTML,
-  }
-  
-  // console.log(updatedData);
-  globalStore = globalStore.map( (blog)=>{
-   if(blog.id === targetID){
-     return {
-       id :blog.id,
-       imageUrl: blog.imageUrl,
-       blogTitle : updatedData.blogTitle,
-       blogType : updatedData.blogType,
-       blogDescription : updatedData.blogDescription,
-     };
-   }
-   return blog;  // important statement
-  });
+	let blogTitle = parentElement.childNodes[5].childNodes[1];
+	let blogDescription = parentElement.childNodes[5].childNodes[3];
+	let blogType = parentElement.childNodes[5].childNodes[5];
+	let submitBtn = parentElement.childNodes[7].childNodes[1];
 
-  updateLocalStorage();
+	const updatedData = {
 
-  blogTitle.setAttribute("contenteditable","false");
-  
-  blogDescription.setAttribute("contenteditable","false");
-  blogType.setAttribute("contenteditable","false");
+		blogTitle: blogTitle.innerHTML,
+		blogDescription: blogDescription.innerHTML,
+		blogType: blogType.innerHTML,
+	}
 
-  // modal added
-  submitBtn.setAttribute("onclick", "openBlog.apply(this, arguments)");
-  submitBtn.setAttribute("data-bs-toggle", "modal");
-  submitBtn.setAttribute("data-bs-target", "#showblog");
+	// console.log(updatedData);
+	globalStore = globalStore.map((blog) => {
+		if (blog.id === targetID) {
+			return {
+				id: blog.id,
+				imageUrl: blog.imageUrl,
+				blogTitle: updatedData.blogTitle,
+				blogType: updatedData.blogType,
+				blogDescription: updatedData.blogDescription,
+			};
+		}
+		return blog; // important statement
+	});
 
-  submitBtn.innerHTML = "Open Blog";
+	updateLocalStorage();
+
+	blogTitle.setAttribute("contenteditable", "false");
+
+	blogDescription.setAttribute("contenteditable", "false");
+	blogType.setAttribute("contenteditable", "false");
+
+	// modal added
+	submitBtn.setAttribute("onclick", "openBlog.apply(this, arguments)");
+	submitBtn.setAttribute("data-bs-toggle", "modal");
+	submitBtn.setAttribute("data-bs-target", "#showblog");
+
+	submitBtn.innerHTML = "Open Blog";
 
 
-  
+
 }
 
-const htmlModalContent = ({ id, blogTitle, blogDescription, imageUrl, blogType }) => {
-  const date = new Date(parseInt(id));
-  return ` <div id=${id}>
+const htmlModalContent = ({
+	id,
+	blogTitle,
+	blogDescription,
+	imageUrl,
+	blogType
+}) => {
+	const date = new Date(parseInt(id));
+	return ` <div id=${id}>
    <img
    src=${imageUrl}
    alt="bg image"
@@ -220,14 +232,17 @@ const htmlModalContent = ({ id, blogTitle, blogDescription, imageUrl, blogType }
    <p class="lead mt-2">
    ${blogDescription}
    </p></div>`;
- };
+};
 
 const openBlog = (event) => {
 
-  event = window.event;
-  const targetID = event.target.id;
+	event = window.event;
+	const targetID = event.target.id;
 
-  const getBlog = globalStore.filter(({ id }) => id === targetID);
-  // console.log(getBlog[0]);
-  blogModal.innerHTML = htmlModalContent(getBlog[0]);
+	const getBlog = globalStore.filter(({
+		id
+	}) => id === targetID);
+	// console.log(getBlog[0]);
+	blogModal.innerHTML = htmlModalContent(getBlog[0]);
 };
+
