4caae3b8ac9f9d39ab87dcb876c32f4b198b9724
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-07-10 08:09:37 +0530

    Added port

--- TRIBUTE PAGE/app.js
+++ TRIBUTE PAGE/app.js
@@ -7,3 +7,7 @@ app.use(express.static("Public"));
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
+
+app.listen(3000, function(req, res) {
+    console.log("Server started running on port 3000");
+})
