d680ebe91291cb90245f1f90dcc9a17a7f029b02
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-07-10 08:09:08 +0530

    Adding get route request

--- TRIBUTE PAGE/app.js
+++ TRIBUTE PAGE/app.js
@@ -3,3 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("Public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
