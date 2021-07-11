3ab57e2e7ded6f85099678b62d04ab4b85e4795c
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-07-11 11:43:35 +0530

    Added expand button

---
+++ Expand-Button/script.js
@@ -0,0 +1,24 @@
+$(".contact-btn").click(function () {
+  $(this).addClass("clicked");
+});
+
+$(".close").click(function (e) {
+  $(".clicked").removeClass("clicked");
+  e.stopPropagation();
+  $(".send-clicked").removeClass("send-clicked");
+  e.stopPropagation();
+});
+
+$(".send-btn").click(function () {
+  $(this).addClass("send-clicked");
+  setTimeout(function () {
+    $(".clicked").removeClass("clicked");
+
+    $(".send-clicked").removeClass("send-clicked");
+  }, 5000);
+});
+
+$(".msg-close").click(function (e) {
+  $(".send-clicked").removeClass("send-clicked");
+  e.stopPropagation();
+});
