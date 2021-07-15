25b9cbc69b9e1eb7ed113527c9fe7f65ccca47d4
Khushi Purwar <52875298+khushi-purwar@users.noreply.github.com>
2021-07-15 19:27:15 +0530

    Update App.js

--- image-finder-app/src/App.js
+++ image-finder-app/src/App.js
@@ -1,7 +1,12 @@
-import {useEffect, useState} from 'react';
+import {
+	useEffect,
+	useState
+} from 'react';
 
 // import from material/ui
-import {Box} from '@material-ui/core'
+import {
+	Box
+} from '@material-ui/core'
 
 //  components
 import Navbar from './Components/Navbar'
@@ -9,47 +14,64 @@ import BreadCrumb from './Components/BreadCrumb';
 import Image from './Components/Image'
 import SnackBar from './Components/SnackBar';
 
-import {getImages} from './Services/api'
+import {
+	getImages
+} from './Services/api'
 
 function App() {
-  const [data, setData] = useState([]);
-  const[count, setCount]  = useState(10);
-  const[text , setText] = useState("mountains");
-  const[open , toggleSnack] = useState(false);
+	const [data, setData] = useState([]);
+	const [count, setCount] = useState(10);
+	const [text, setText] = useState("mountains");
+	const [open, toggleSnack] = useState(false);
 
-  useEffect( ()=>{
+	useEffect(() => {
 
-     if(count<3 || count>200){
-     toggleSnack(true);
-     return;
-     }
-     
-     toggleSnack(false);
+		if (count < 3 || count > 200) {
+			toggleSnack(true);
+			return;
+		}
 
-    getImages(text,count).then( response => {
-      setData(response.data.hits)
-      // console.log(response.data.hits);
-    })
-  } , [text,count])
+		toggleSnack(false);
+
+		getImages(text, count).then(response => {
+			setData(response.data.hits)
+			// console.log(response.data.hits);
+		})
+	}, [text, count])
 
 
-  const onTextChange = (text)=>{
-    setText(text);
-  }
+	const onTextChange = (text) => {
+		setText(text);
+	}
 
-  
-  const onCountChange = (count)=>{
-    setCount(count);
-  }
 
-  return (
-    <Box>
-     <Navbar />
-     <BreadCrumb onTextChange= {onTextChange} onCountChange ={onCountChange}/>
-     <Image data = {data} />
-     <SnackBar open={open} toggleSnack={toggleSnack}/>
-    </Box>
-  );
+	const onCountChange = (count) => {
+		setCount(count);
+	}
+
+	return ( 
+		<Box >
+		<Navbar / >
+		<BreadCrumb onTextChange = {
+			onTextChange
+		}
+		onCountChange = {
+			onCountChange
+		}
+		/> 
+		<Image data = {
+			data
+		}
+		/> 
+		<SnackBar open = {
+			open
+		}
+		toggleSnack = {
+			toggleSnack
+		}
+		/> 
+		</Box>
+	);
 }
 
 export default App;
