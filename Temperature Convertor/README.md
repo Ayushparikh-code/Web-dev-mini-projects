26ba402671548a59ba9df9239ec7a3d41111b476
Kaushal Joshi <7joshikaushal@gmail.com>
2021-07-17 04:14:05 +0530

    project finished

---
+++ Temperature Convertor/README.md
@@ -0,0 +1,23 @@
+# Temperature Convertor
+
+This webpage converts temperatures from and to Celcius, Fahrenheit and Kelvin.
+
+### Tech Stack
+
+- HTML5
+- Bulma CSS
+- Vanilla JS
+
+### Prequisites
+
+- Git and GitHub installed in the machine in order to clone the repo.
+
+### How to Install
+
+- Open terminal / CMD to access git commands.
+- Enter this command:
+  `https://github.com/joshi-kaushal/Web-dev-mini-projects.git`
+- switch to _temp-convertor_ branch and open the folder named _Temperature Convertor_
+- run `index.html` file
+
+### Screenshots
--- Temperature Convertor/script.js
+++ Temperature Convertor/script.js
@@ -1,62 +1,71 @@
-let done = document.getElementById('converted-value')
+let results = document.getElementById('converted-value')
 
+// On change event for the second SELECT attribute
 to.onchange = () => {
-    done.innerHTML = ""
+    results.innerHTML = ""
+    results.style.color = 'black'
+    
     let from = document.getElementById('from')
     let to = document.getElementById('to')
+    let input = document.getElementById('input').value
 
     let fromValue = from.value
     let toValue = to.value
 
+    // Checking if both are same
     if(fromValue === toValue) {
-        done.innerHTML = "Units should be different"
+        results.style.color = 'red'
+        results.innerHTML = "CONVERSION UNITS SHOULD BE DIFFERENT!"
     }
 
+    // Checking the input and passing it to appropriate functions
     if(fromValue === 'celsius' && toValue === 'kelvin') {
-        celsiusToKelvin(fromValue, toValue) 
+        celsiusToKelvin(input) 
     }
 
     if(fromValue === 'celsius' && toValue === 'fahrenheit') {
-        celsiusToFahrenheit(fromValue, toValue) 
+        celsiusToFahrenheit(input) 
     }
     
     if(fromValue === 'fahrenheit' && toValue === 'kelvin') {
-        fahrenheitToKelvin(fromValue, toValue) 
+        fahrenheitToKelvin(input) 
     }
 
     if(fromValue === 'fahrenheit' && toValue === 'celsius') {
-        fahrenheitToCelsius(fromValue, toValue) 
+        fahrenheitToCelsius(input) 
     }
     
     if(fromValue === 'kelvin' && toValue === 'fahrenheit') {
-        KelvinToFahrenheit(fromValue, toValue) 
+        KelvinToFahrenheit(input) 
     }
 
     if(fromValue === 'kelvin' && toValue === 'celsius') {
-        KelvinToCelsius(fromValue, toValue) 
+        KelvinToCelsius(input) 
     }
 }
 
-const celsiusToKelvin = (fromValue, toValue) => {
-    alert(`${fromValue}, ${toValue}`)
+
+// Conversion of input values
+const celsiusToKelvin = (input) => {
+    results.innerHTML = `The final converted value is ${parseInt(input + 273.15, 10).toFixed(2)}`
 }
 
-const celsiusToFahrenheit = (fromValue, toValue) => {
-    alert(`${fromValue}, ${toValue}`)
+const celsiusToFahrenheit = (input) => {
+    results.innerHTML = `The final converted value is ${parseInt(input * (9 / 5) + 32, 10).toFixed(2)}`    
 }
 
-const fahrenheitToKelvin = (fromValue, toValue) => {
-    alert(`${fromValue}, ${toValue}`)
+const fahrenheitToKelvin = (input) => {
+    results.innerHTML = `The final converted value is ${parseInt((input + 459.67) * (5 / 9), 10).toFixed(2)}`
 }
 
-const fahrenheitToCelsius = (fromValue, toValue) => {
-    alert(`${fromValue}, ${toValue}`)
+const fahrenheitToCelsius = (input) => {
+    results.innerHTML = `The final converted value is ${parseInt((input - 32) * (5 / 9), 10).toFixed(2)}`
 }
 
-const KelvinToFahrenheit = (fromValue, toValue) => {
-    alert(`${fromValue}, ${toValue}`)
+const KelvinToFahrenheit = (input) => {
+    results.innerHTML = `The final converted value is ${parseInt((input * 1.8) - 459.67, 10).toFixed(2)}`    
 }
 
-const KelvinToCelsius = (fromValue, toValue) => {
-    alert(`${fromValue}, ${toValue}`)
+const KelvinToCelsius = (input) => {
+    results.innerHTML = `The final converted value is ${parseInt(input - 273.15, 10).toFixed(2)}`
 }
