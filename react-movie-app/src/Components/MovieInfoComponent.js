d748dde7da5c8522398f1ba1bc698397ae602014
Khushi Purwar <52875298+khushi-purwar@users.noreply.github.com>
2021-07-24 00:48:09 +0530

    Update MovieInfoComponent.js

--- react-movie-app/src/Components/MovieInfoComponent.js
+++ react-movie-app/src/Components/MovieInfoComponent.js
@@ -1,8 +1,13 @@
-import React ,{useState, useEffect} from 'react';
+import React, {
+	useState,
+	useEffect
+} from 'react';
 import styled from "styled-components";
 import Axios from 'axios';
 
-import { API_KEY } from "../App";
+import {
+	API_KEY
+} from "../App";
 
 const Container = styled.div`
   display: flex;
@@ -65,64 +70,97 @@ const Close = styled.span`
 
 const MovieInfoComponent = (props) => {
 
-    const [movieInfo, setMovieInfo] = useState();
+	const [movieInfo, setMovieInfo] = useState();
 
-    const {selectedMovie} = props;
+	const {
+		selectedMovie
+	} = props;
 
-    useEffect (()=> {
-        Axios.get(
-            `https://www.omdbapi.com/?i=${selectedMovie}&apikey=${API_KEY}`,
-          ).then((response) => setMovieInfo(response.data));
-    },[selectedMovie]);
+	useEffect(() => {
+		Axios.get(
+			`https://www.omdbapi.com/?i=${selectedMovie}&apikey=${API_KEY}`,
+		).then((response) => setMovieInfo(response.data));
+	}, [selectedMovie]);
 
-    return (
-        <Container>
-            {movieInfo ? (
-                <>
-            <CoverImage src={movieInfo?.Poster} alt="poster" />
-            <InfoColumn>
-            <MovieName>
-              {movieInfo?.Type}: <span>{movieInfo?.Title}</span>
-            </MovieName>
-            <MovieInfo>
-              IMDB Rating: <span>{movieInfo?.imdbRating}</span>
-            </MovieInfo>
-            <MovieInfo>
-              Year: <span>{movieInfo?.Year}</span>
-            </MovieInfo>
-            <MovieInfo>
-              Language: <span>{movieInfo?.Language}</span>
-            </MovieInfo>
-            <MovieInfo>
-              Rated: <span>{movieInfo?.Rated}</span>
-            </MovieInfo>
-            <MovieInfo>
-              Released: <span>{movieInfo?.Released}</span>
-            </MovieInfo>
-            <MovieInfo>
-              Runtime: <span>{movieInfo?.Runtime}</span>
-            </MovieInfo>
-            <MovieInfo>
-              Genre: <span>{movieInfo?.Genre}</span>
-            </MovieInfo>
-            <MovieInfo>
-              Director: <span>{movieInfo?.Director}</span>
-            </MovieInfo>
-            <MovieInfo>
-              Actors: <span>{movieInfo?.Actors}</span>
-            </MovieInfo>
-            <MovieInfo>
-              Plot: <span>{movieInfo?.Plot}</span>
-            </MovieInfo>
-            </InfoColumn>
-            <Close onClick={() => props.onMovieSelect()}>X</Close>
-            </>
-            ) : (
-               
-                <img src="http://www.downgraf.com/wp-content/uploads/2014/09/01-progress.gif" atl="loading" height="200" />
-              )}
-        </Container>
-    )
+	return ( <Container > {
+			movieInfo ? ( 
+      <>
+				<CoverImage src = {
+					movieInfo?.Poster
+				}
+				alt = "poster" / >
+				<InfoColumn >
+				<MovieName > {
+					movieInfo?.Type
+				}: < span > {
+					movieInfo?.Title
+				} < /span> 
+        </MovieName> <
+				MovieInfo >
+				IMDB Rating: < span > {
+					movieInfo?.imdbRating
+				} < /span> 
+        </MovieInfo> 
+        <MovieInfo >
+				Year: < span > {
+					movieInfo?.Year
+				} < /span> 
+        </MovieInfo> 
+        <MovieInfo >
+				Language: < span > {
+					movieInfo?.Language
+				} < /span> 
+        </MovieInfo> 
+        <MovieInfo >
+				Rated: < span > {
+					movieInfo?.Rated
+				} < /span> <
+				/MovieInfo> <
+				MovieInfo >
+				Released: < span > {
+					movieInfo?.Released
+				} < /span> <
+				/MovieInfo> <
+				MovieInfo >
+				Runtime: < span > {
+					movieInfo?.Runtime
+				} < /span> <
+				/MovieInfo> <
+				MovieInfo >
+				Genre: < span > {
+					movieInfo?.Genre
+				} < /span> <
+				/MovieInfo> <
+				MovieInfo >
+				Director: < span > {
+					movieInfo?.Director
+				} < /span> <
+				/MovieInfo> <
+				MovieInfo >
+				Actors: < span > {
+					movieInfo?.Actors
+				} < /span> <
+				/MovieInfo> <
+				MovieInfo >
+				Plot: < span > {
+					movieInfo?.Plot
+				} < /span> <
+				/MovieInfo> <
+				/InfoColumn> <
+				Close onClick = {
+					() => props.onMovieSelect()
+				} > X < /Close> <
+				/>
+			) : (
+
+				<
+				img src = "http://www.downgraf.com/wp-content/uploads/2014/09/01-progress.gif"
+				atl = "loading"
+				height = "200" / >
+			)
+		} <
+		/Container>
+	)
 }
 
-export default MovieInfoComponent
+export default MovieInfoComponent;
