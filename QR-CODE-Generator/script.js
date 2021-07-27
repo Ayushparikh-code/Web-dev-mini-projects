de488a569cb711164920fe0dc9f716ff3baa0c47
Khushi Purwar <52875298+khushi-purwar@users.noreply.github.com>
2021-07-27 15:31:23 +0530

    update script.js

--- QR-CODE-Generator/script.js
+++ QR-CODE-Generator/script.js
@@ -5,17 +5,17 @@ const toast = document.querySelector('#toast');
 
 btn.addEventListener('click', generate);
 
-function generate(){
-    const data = input.value;
-    const URL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`;
-    code.src = URL;
-   
-    toastDiv();
+function generate() {
+	const data = input.value;
+	const URL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`;
+	code.src = URL;
+
+	toastDiv();
 }
 
-function toastDiv(){
-    toast.className = "show";
-    setTimeout( function(){
-        toast.className = toast.className.replace("show", "");
-    } , 2000)
+function toastDiv() {
+	toast.className = "show";
+	setTimeout(function() {
+		toast.className = toast.className.replace("show", "");
+	}, 2000)
 }
