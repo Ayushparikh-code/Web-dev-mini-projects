a8cc7f1d20f1bc077a60dde445e81f305374c632
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-07-28 12:39:42 +0530

    Updated main.js

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Main from './components/Main';
-import Header from './components/Header';
-import Footer from "./components/Footer";
+import Main from './Components/Main';
+import Header from './Components/Header';
+import Footer from "./Components/Footer";
 
 function App() {
   return (
--- news-app/src/Components/Main.js
+++ news-app/src/Components/Main.js
@@ -3,7 +3,6 @@ import "./Main.css";
 
 const Main = () => {
   const [news, setNews] = useState([]);
-  const [tnews, setTNews] = useState([]);
 
   const loadNews = async () => {
     const response = await fetch(
@@ -13,17 +12,9 @@ const Main = () => {
     setNews(data.articles);
   };
 
-  const loadTesla = async () => {
-    const response = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2021-06-19&sortBy=publishedAt&apiKey=4fc8ad23db54411c8dce19801bccff07");
-    const data = await response.json();
-    console.log(data.articles);
-    setTNews(data.articles);
-    console.log(tnews)
-  }
 
   useEffect(() => {
     loadNews();
-    loadTesla();
   }, []);
 
 
@@ -46,26 +37,7 @@ const Main = () => {
           );
         })}
         </div>
-<div className="tesla-main">
-      <h2 className="tesla-heading">Tesla News</h2>
-      
-      <div className="tesla-container">
-        {tnews.map(data => {
-          return(
-          <div className="card1">
-            <div className="image">
-              <img src={data.urlToImage} alt="" />
-            </div>
-            <div className="info">
-              <h3>{data.title}</h3>
 
-              <p>{data.description}</p>
-            </div>
-          </div>
-          );
-        })}
-      </div>
-    </div>
     </div>
    
   );
