e4fbfc8997e531c19cc0cc86a0ee11655d734320
Khushi Purwar <52875298+khushi-purwar@users.noreply.github.com>
2021-08-04 00:52:38 +0530

    beautify js file

--- VAT Calculator/script.js
+++ VAT Calculator/script.js
@@ -5,48 +5,48 @@ const incAmount = document.querySelector("#inc-amount");
 const calcBtn = document.querySelector("#calc-btn")
 const resetBtn = document.querySelector("#reset-btn")
 
-function VATInclusiveAmount(price, rate){
-    let vatAddedPrice =  Number((price * (rate/100)));
-    vatAdded.value = vatAddedPrice.toFixed(2);
-    let totalAmount = Number(price) + vatAddedPrice ;
-    return totalAmount;
+function VATInclusiveAmount(price, rate) {
+	let vatAddedPrice = Number((price * (rate / 100)));
+	vatAdded.value = vatAddedPrice.toFixed(2);
+	let totalAmount = Number(price) + vatAddedPrice;
+	return totalAmount;
 }
 
-calcBtn.addEventListener('click', function(){
-    if(netPrice.value === "" || isNaN(netPrice.value)){
-        // show red border
-        netPrice.style.border = "1px solid red";
+calcBtn.addEventListener('click', function() {
+	if (netPrice.value === "" || isNaN(netPrice.value)) {
+		// show red border
+		netPrice.style.border = "1px solid red";
 
-        // removing red border after 1.5s
-        setTimeout(function(){
-            netPrice.style.border = "1px solid transparent";
-        }, 1500);
-        
-        netPrice.value = "";
-    }
+		// removing red border after 1.5s
+		setTimeout(function() {
+			netPrice.style.border = "1px solid transparent";
+		}, 1500);
 
-     if(vatRate.value === "" || isNaN(vatRate.value)){
-        // show red border
-        vatRate.style.border = "1px solid red";
+		netPrice.value = "";
+	}
 
-        // removing red border after 1.5s
-        setTimeout(function(){
-            vatRate.style.border = "1px solid transparent";
-        }, 1500);
-        
-        // remove wrong value
-        vatRate.value = "";
-    }
+	if (vatRate.value === "" || isNaN(vatRate.value)) {
+		// show red border
+		vatRate.style.border = "1px solid red";
 
-    let finalAmount = VATInclusiveAmount((netPrice.value), (vatRate.value));
-    incAmount.value = finalAmount;
-    
+		// removing red border after 1.5s
+		setTimeout(function() {
+			vatRate.style.border = "1px solid transparent";
+		}, 1500);
+
+		// remove wrong value
+		vatRate.value = "";
+	}
+
+	let finalAmount = VATInclusiveAmount((netPrice.value), (vatRate.value));
+	incAmount.value = finalAmount;
+
 
 })
 
-resetBtn.addEventListener("click", function(){
-    netPrice.value="";
-    vatRate.value="";
-    vatAdded.value = "";
-    incAmount.value="";
+resetBtn.addEventListener("click", function() {
+	netPrice.value = "";
+	vatRate.value = "";
+	vatAdded.value = "";
+	incAmount.value = "";
 })
