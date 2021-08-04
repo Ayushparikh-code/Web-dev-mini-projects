931cb1ef886996652bb35b2bbddec0f2a91d695c
Supritha <supri161@gmail.com>
2021-08-04 18:36:25 +0530

    Update 2

--- Password Strength Meter/script.js
+++ Password Strength Meter/script.js
@@ -12,59 +12,62 @@ let regExpStrong = /.[!,@,#,$,%,^,&,*,(,)]/;
 
 
 
-function trigger() {
+document.getElementById("trigger").addEventListener("keyup", function() {
 	if (input.value != "") {
 		indicator.style.display = "flex";
 
-		if (input.value.length <= 3 && (input.value.match(regExpWeak) || input.value.match(regExpMedium) || input.value.match(regExpStrong))) no = 1;
 
-		if (input.value.length >= 6 && ((input.value.match(regExpWeak) && input.value.match(regExpMedium)) || (input.value.match(regExpMedium) && input.value.match(regExpStrong)) || (input.value.match(regExpWeak) && input.value.match(regExpStrong)))) no = 2;
-
-		if (input.value.length >= 6 && input.value.match(regExpWeak) && input.value.match(regExpMedium) && input.value.match(regExpStrong)) no = 3;
-
-		if (no == 1) {
-			weak.classList.add("active");
-			text.style.display = "block";
-			text.textContent = "Your password is too weak";
-			text.classList.add("weak");
-		}
-
-		if (no == 2) {
-			medium.classList.add("active");
-			text.textContent = "Your password is average";
-			text.classList.add("medium");
-		} else {
-			medium.classList.remove("active");
-			text.classList.remove("medium");
-		}
-
-		if (no == 3) {
-			medium.classList.add("active");
-			strong.classList.add("active");
-			text.style.display = "block";
-			text.textContent = "Your password is strong";
-			text.classList.add("strong");
-		} else {
-			strong.classList.remove("active");
-			text.classList.remove("strong");
-		}
-
-		showBtn.style.display = "block";
-		showBtn.onclick = function() {
-			if (input.type == "password") {
-				input.type = "text";
-				showBtn.textContent = "HIDE";
-				showBtn.style.color = "#23ad5c";
-			} else {
-				input.type = "password";
-				showBtn.textContent = "SHOW";
-				showBtn.style.color = "#000";
-			}
-		}
 	} else {
 		indicator.style.display = "none";
 		text.style.display = "none";
 		showBtn.style.display = "none";
 	}
-}
+
+	if (input.value.length <= 3 && (input.value.match(regExpWeak) || input.value.match(regExpMedium) || input.value.match(regExpStrong))) no = 1;
+
+	if (input.value.length >= 6 && ((input.value.match(regExpWeak) && input.value.match(regExpMedium)) || (input.value.match(regExpMedium) && input.value.match(regExpStrong)) || (input.value.match(regExpWeak) && input.value.match(regExpStrong)))) no = 2;
+
+	if (input.value.length >= 6 && input.value.match(regExpWeak) && input.value.match(regExpMedium) && input.value.match(regExpStrong)) no = 3;
+
+	if (no == 1) {
+		weak.classList.add("active");
+		text.style.display = "block";
+		text.textContent = "Your password is too weak";
+		text.classList.add("weak");
+	}
+
+	if (no == 2) {
+		medium.classList.add("active");
+		text.textContent = "Your password is average";
+		text.classList.add("medium");
+	} else {
+		medium.classList.remove("active");
+		text.classList.remove("medium");
+	}
+
+	if (no == 3) {
+		medium.classList.add("active");
+		strong.classList.add("active");
+		text.style.display = "block";
+		text.textContent = "Your password is strong";
+		text.classList.add("strong");
+	} else {
+		strong.classList.remove("active");
+		text.classList.remove("strong");
+	}
+
+	showBtn.style.display = "block";
+	showBtn.onclick = function() {
+		if (input.type == "password") {
+			input.type = "text";
+			showBtn.textContent = "HIDE";
+			showBtn.style.color = "#23ad5c";
+		} else {
+			input.type = "password";
+			showBtn.textContent = "SHOW";
+			showBtn.style.color = "#000";
+		}
+	}
+
+});
 
