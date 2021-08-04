20c97b9c2e6b60d1908d7ac1ec66f999408d786f
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-08-04 13:15:32 +0530

    added my pizza app@

---
+++ react-tailwind-pizza-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="bg-gray-800 h-16 flex items-center justify-center text-white">
+            <p>Copyright &copy; 2021 PIZZAGHAR | All rights reserved.</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ react-tailwind-pizza-app/src/pages/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+        <div className="bg-yellow-500 h-screen w-screen flex justify-center items-center overflow-x-hidden">
+            <h1 className="text-white font-mono text-7xl font-bold uppercase">Menu Page</h1>
+        </div>
+    )
+}
+
+export default Menu
