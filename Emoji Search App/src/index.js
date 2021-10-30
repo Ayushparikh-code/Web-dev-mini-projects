5b5b95ae1013000218930475a2020eac1184c4aa
Anju Priya V <84177086+anjupriya-v@users.noreply.github.com>
2021-10-30 19:30:35 +0530

    Add files via upload

---
+++ Emoji Search App/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
