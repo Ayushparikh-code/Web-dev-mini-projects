8a8083463c10674407d646ede5508fbd4bce37c6
Priyanshu Kumar <110731419+PRIYANSHUKUMAR123451@users.noreply.github.com>
2024-10-10 00:04:32 +0530

    Hospital Tracker App to track nearest location.

--- Ecommerce Website/search_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SearchAppConfig(AppConfig):
-    name = 'search_app'
--- Ecommerce Website/shop/context_processors.py
+++
@@ -1,4 +0,0 @@
-from . models import Category
-def menu_links(request):
-    links=Category.objects.all()
-    return dict(links=links)
