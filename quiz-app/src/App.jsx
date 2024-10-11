5e4e865c2d9b8ee88f8f5f343099ad59a994b72a
Temmarie <temmar_whyte16@yahoo.com>
2024-10-11 13:07:54 +0100

    Add Instructions component and Welcome Screen components

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './index.css'
 import Navbar from './components/Navbar'
+import Instructions from './components/Instructions';
+import WelcomeScreen from './components/Welcome';
 function App() {
  
 
   return (
     <>
       <Navbar />
+      <WelcomeScreen />
+      <Instructions />
      </>
   );
 };
