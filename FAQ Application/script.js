66944274eda52ecae114123605e3585af6065aeb
Iniyal Palanisamy <123928113+IniyalPalanisamy@users.noreply.github.com>
2024-10-19 08:32:42 +0530

    Update script.js

--- FAQ Application/script.js
+++ FAQ Application/script.js
@@ -1,8 +1,9 @@
 const faqs = document.querySelectorAll('.faq');
 
 faqs.forEach((faq) => {
-	faq.addEventListener('click', () => {
-		faq.classList.toggle("active");
-	});
-})
-
+  faq.addEventListener('click', () => {
+    faq.classList.toggle("active");
+    const content = faq.querySelector('.faq-content'); // assuming the content is in an element with class 'faq-content'
+    content.classList.toggle("active");
+  });
+});
