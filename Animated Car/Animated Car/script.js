ca723d8552d65314701f66563e4a2f4405f85fe1
Aathish101 <aathishvs2@gmail.com>
2024-10-19 09:15:53 +0530

    Update script.js

--- Animated Car/Animated Car/script.js
+++ Animated Car/Animated Car/script.js
@@ -1,21 +1,32 @@
-// const audio = new Audio('caraudio.mp3');
-// var resp = audio.play();
-
-// if(resp != undefined){
-//     resp.then(_ => {
-//         //autoplay starts
-//     }).catch(error => {
-//         //shoe error
-//     });
-// }
-// audio.loop()=true;
-
 var audio = document.createElement("audio");
 document.body.appendChild(audio);
-audio.src="caraudio.mp3"
+audio.src = "caraudio.mp3";
 
+let isMoving = false;
 
-document.body.addEventListener("mousemove", function(){
-    audio.play();
-    audio.loop();
-})
+// Start playing the audio when the mouse moves
+document.body.addEventListener("mousemove", function() {
+    if (!isMoving) {
+        audio.play();
+        audio.loop = true; // Set the audio to loop
+        isMoving = true;   // Indicate that the car is moving
+    }
+});
+
+// Stop playing the audio when the left mouse button is released
+document.body.addEventListener("mouseup", function(event) {
+    if (event.button === 0) { // 0 is the left mouse button
+        audio.pause();
+        audio.currentTime = 0; // Optionally reset audio to the beginning
+        isMoving = false;      // Indicate that the car has stopped
+    }
+});
+
+// Optional: Start playing audio again if the mouse is moved while stopped
+document.body.addEventListener("mousedown", function() {
+    if (!isMoving) {
+        audio.play();
+        audio.loop = true;
+        isMoving = true;
+    }
+});
