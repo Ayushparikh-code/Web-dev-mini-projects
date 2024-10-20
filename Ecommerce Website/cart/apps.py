85b1278ca1b0ce854e71e6309729fde793ee3e20
RajdeepDey010 <rdey5486@gmail.com>
2024-10-20 16:27:14 +0530

    added LoginForm

--- Ecommerce Website/cart/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CartConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cart'
--- FAQ Application/script.js
+++
@@ -1,8 +0,0 @@
-const faqs = document.querySelectorAll('.faq');
-
-faqs.forEach((faq) => {
-	faq.addEventListener('click', () => {
-		faq.classList.toggle("active");
-	});
-})
-
