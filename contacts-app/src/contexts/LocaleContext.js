c66873e7388e7c7f36a55cec49d21c6fd76c03b0
Jhostin23-06 <bravojhostin232001@gmail.com>
2025-01-10 11:27:55 -0500

    first commit

--- contacts-app/src/contexts/LocaleContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const LocaleContext = createContext();
-
-export const LocaleProvider = LocaleContext.Provider;
-export const LocaleConsumer = LocaleContext.Consumer;
-export default LocaleContext;
